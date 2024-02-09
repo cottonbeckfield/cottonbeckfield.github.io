3b6e9ab7eb276764b7ed48294d71395356112791
Cotton <cotton@dawnbreaker.lan>
2024-02-09 15:04:17 -0600

    V 1.0.0

---
+++ js/main.js
@@ -0,0 +1,49 @@
+var hashTagActive = "";
+$(".scroll").click(function (event) {
+    if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+        event.preventDefault();
+        //calculate destination place
+        var dest = 0;
+        if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+            dest = $(document).height() - $(window).height();
+        } else {
+            dest = $(this.hash).offset().top;
+        }
+        //go to destination
+        $('html,body').animate({
+            scrollTop: dest
+        }, 1000, 'swing');
+        hashTagActive = this.hash;
+    }
+});
+
+$(function(){
+    $(document).on( 'scroll', function(){
+        if ($(window).scrollTop() > 100) {
+            $('.scroll-top-wrapper').addClass('show');
+        } else {
+            $('.scroll-top-wrapper').removeClass('show');
+        }
+    });
+    $('.scroll-top-wrapper').on('click', scrollToTop);
+});
+ 
+function scrollToTop() {
+    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $('body');
+    offset = element.offset();
+    offsetTop = offset.top;
+    $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
+
+$(document).ready(function(e) {
+    $('img[usemap]').rwdImageMaps();
+});
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-66596936-1', 'auto');
+ga('send', 'pageview');
